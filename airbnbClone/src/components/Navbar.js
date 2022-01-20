b6eaf96e061317e267822a3a8cfde47943434b98
Jason Evans <evans.jason@hotmail.co.uk>
2022-01-20 20:35:46 +0000

    starbucks clone and air bnb clone added

---
+++ airbnbClone/src/components/Navbar.js
@@ -0,0 +1,9 @@
+import react from 'react' 
+
+export default function Navbar() {
+    return(
+        <nav>
+            <img src="../images/airbnb-logo.png" alt="Logo" className='nav--logo' />
+        </nav>
+    )
+}
